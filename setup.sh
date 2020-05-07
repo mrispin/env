71745598b2f325b84b1e73bd84c97ff320deded4
Mark Rispin <mark.rispin@iquate.com>
2020-05-07 12:31:16 +0100

    Add link to .zshrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ln -sf ~/mrispin/env/home/.zshrc ~/.zshrc
 sudo apt update && sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -8,11 +9,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Setup kubectx
 git clone https://github.com/ahmetb/kubectx ~/kubectx
 mkdir -p ~/bin
-ln -s ~/kubectx/kubectx ~/bin/kubectx
-ln -s ~/kubectx/kubens ~/bin/kubens
+ln -sf ~/kubectx/kubectx ~/bin/kubectx
+ln -sf ~/kubectx/kubens ~/bin/kubens
 
 mkdir -p ~/.oh-my-zsh/completions
 chmod -R 755 ~/.oh-my-zsh/completions
-ln -s ~/kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
-ln -s ~/kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+ln -sf ~/kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
+ln -sf ~/kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
 
